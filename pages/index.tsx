1a3cdaa2740fd22e076b6007958acd463eb17832
Richard Thombs <richard@gearstone.uk>
2021-03-03 13:16:14 +0000

    Bit more splash

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,31 @@ import Head from 'next/head'
 
 export default function Home() {
 	return (
-		<div className="container mx-auto sm:px-6 lg:px-8">
-			<div className="relative min-h-screen">
-				<div className="absolute inset-0">
-					<img className="h-full w-full object-cover" src="/splash.jpg" alt="Picture of a camper" />
+		<div className="min-h-screen bg-gray-900">
+			<div className="container mx-auto">
+
+				<div className="relative min-h-screen">
+					<div className="absolute inset-0 bg-blue-200">
+						<img className="h-full w-full object-cover" src="/splash.jpg" alt="Picture of a camper" />
+					</div>
+
+					<div className="absolute top-8 w-full">
+						<h1 className="text-center text-white tracking-tight text-4xl font-extrabold sm:text-5xl lg:text-6xl">
+							<div>Hit the open road and</div>
+							<div>sleep under the stars</div>
+						</h1>
+					</div>
+
+					<div className="absolute bottom-8 w-full text-center">
+						<svg className="inline text-white opacity-50 w-12 sm:w-24" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+						</svg>
+					</div>
+
 				</div>
 
-				<div className="absolute bottom-32 sm:bottom-8 w-full">
-					<h1 className="text-center text-white opacity-90 text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
-						<div className="text-white opacity-90">Hit the open road and</div>
-						<div className="text-white opacity-90">sleep under the stars</div>
-					</h1>
+				<div className="h-96 bg-white">
 				</div>
-
 			</div>
 		</div>
 	);
